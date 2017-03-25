303e279a793a8573bb657ccf52907c3076c55e50
Ashok Soni <ashok.soni@gmail.com>
2017-03-25 15:26:34 +0530

    SONY Framework support
    Now you can install Sony apps from Play store easily.
    
    All credits for the zip file to z3n17h@xda
    
    Change-Id: I897f9e7d3f0035d3ebc7f753ff6e1f22e9d82e00

---
+++ overlay/vendor/cm/config/common.mk
@@ -0,0 +1,8 @@
+# SONY Framework 
+PRODUCT_COPY_FILES += \
+    device/oneplus/oneplus3/overlay/vendor/cm/prebuilt/common/etc/permissions/com.sony.device.xml:system/etc/permissions/com.sony.device.xml \
+    device/oneplus/oneplus3/overlay/vendor/cm/prebuilt/common/etc/permissions/com.sonyericsson.colorextraction.xml:system/etc/permissions/com.sonyericsson.colorextraction.xml \
+    device/oneplus/oneplus3/overlay/vendor/cm/prebuilt/common/etc/permissions/com.sonymobile.home.resourceprovider.xml:system/etc/permissions/com.sonymobile.home.resourceprovider.xml \
+    device/oneplus/oneplus3/overlay/vendor/cm/prebuilt/common/framework/com.sony.device.jar:system/framework/com.sony.device.jar \
+    device/oneplus/oneplus3/overlay/vendor/cm/prebuilt/common/framework/com.sonyericsson.colorextraction_impl.jar:system/framework/com.sonyericsson.colorextraction_impl.jar \
+    device/oneplus/oneplus3/overlay/vendor/cm/prebuilt/common/framework/com.sonymobile.home.resourceprovider.jar:system/framework/com.sonymobile.home.resourceprovider.jar
